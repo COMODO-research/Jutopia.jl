6a9d980021e9ff0c17f5acf2aff77e77c55a2f26
Aminofa70 <amin.alibakhshi@upm.es>
2025-10-10 16:20:32 +0200

    add functions for fem and top, add first example

--- src/Jutopia.jl
+++ src/Jutopia.jl
@@ -1,5 +1,25 @@
 module Jutopia
+using Ferrite
+using NearestNeighbors
+using Printf
 
-greet() = print("Hello World!")
+export twoD, Traction, Nodal, threeD
+############################################################
+###### fem
+export get_material_matrix
+export assemble_cell!
+export assemble_global!
+export assemble_external_forces!
+export compliance_and_sensitivity
+export vertexdofs , nodeid_to_vertexindex, apply_nodal_force!
 
+########## top 
+export get_centroid, build_centroid_matrix, create_optimized_sensitivity_filter, apply_sensitivity_filter
+export calculate_cell_volume, calculate_all_cell_volumes, update_density
+
+#### solver
+export run_fem
+export run_optimization
+
+include("functions.jl")
 end # module Jutopia
