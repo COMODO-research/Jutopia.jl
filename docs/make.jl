1ff4a25b25534d016f01f3ccc625f7843d29c091
Aminofa70 <amin.alibakhshi@upm.es>
2025-10-11 18:35:34 +0200

    set up docs using DocumenterVitepress.jl

---
+++ docs/make.jl
@@ -0,0 +1,22 @@
+using Documenter
+using DocumenterVitepress
+using Jutopia
+
+makedocs(
+    format = DocumenterVitepress.MarkdownVitepress(
+        repo = "https://github.com/COMODO-research/Jutopia.jl.git"
+    ),
+    sitename = "Jutopia.jl",
+    modules = [Jutopia],
+    pages = [
+        "Home" => "index.md",
+    ]
+)
+
+DocumenterVitepress.deploydocs(
+    repo = "github.com/COMODO-research/Jutopia.jl.git",
+    target = "build",
+    devbranch = "main",
+    push_preview = true,
+)
+
---
+++ docs/src/index.md
@@ -0,0 +1,15 @@
+```@meta
+CurrentModule = Jutopia
+```
+
+# ComodoFerrite
+
+Documentation for [Jutopia](https://github.com/COMODO-research/Jutopia.jl).
+
+
+```@index
+```
+
+```@autodocs
+Modules = [Jutopia]
+```
